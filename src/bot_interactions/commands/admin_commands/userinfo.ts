6d31380b3e5add2784772ce24fec00b6fdf0bc4a
lukasabbe <67807954+lukasabbe@users.noreply.github.com>
2024-09-23 20:13:57 +0200

    added the last commands

---
+++ src/bot_interactions/commands/admin_commands/userinfo.ts
@@ -0,0 +1,44 @@
+import { CommandInteraction, GuildMember, PresenceStatus, SlashCommandBuilder } from "discord.js";
+import { Command } from "../../../classes/command";
+import { EmbedBuilder } from "@discordjs/builders";
+
+export default class UserInfoCommand implements Command {
+    name = "userinfo";
+    ephemeral = false;
+    description = "Hämta information om en användare";
+    aliases = [];
+    data = new SlashCommandBuilder()
+        .setName(this.name)
+        .setDescription(this.description)
+        .addUserOption((option) =>
+            option
+                .setName("user")
+                .setDescription("Personen du vill hämta information om")
+                .setRequired(true),
+        );
+    async execute(interaction: CommandInteraction) {
+        const member = interaction.options.get("user", true).member as GuildMember;
+
+        const roles = member.roles.cache
+            .filter((role) => role.id !== interaction.guild?.id)
+            .map((role) => role.toString())
+            .join(", ");
+
+        const embed = new EmbedBuilder()
+            .setTitle("Användarinformation | " + member.user.username)
+            .setThumbnail(member.user.displayAvatarURL())
+            .addFields(
+                {name:"Användarnamn", value: member.user.username},
+                {name:"ID", value: member.id},
+                {name:"Status", value: member.presence?.status as PresenceStatus},
+                {name:"Roller", value: roles || "Inga roller"}
+            )
+            .setTimestamp()
+            .setFooter({
+                text: this.name,
+                iconURL: interaction.client.user.avatarURL()?.toString(),
+            });
+
+        await interaction.editReply({ embeds: [embed] });
+    }
+}
---
+++ src/bot_interactions/commands/admin_commands/warn.ts
@@ -0,0 +1,74 @@
+import { CommandInteraction, GuildMember, PermissionFlagsBits, SlashCommandBuilder } from "discord.js";
+import { Command } from "../../../classes/command";
+import { GamerBotAPIInstance } from "../../..";
+import { ModLog } from "../../../classes/modlog";
+import { CreateModLogEmbed } from "../../../functions/createEmbed";
+
+export default class WarnCommand implements Command {
+    name = "warn";
+    ephemeral = false;
+    description = "Varna en person!";
+    aliases = [];
+    data = new SlashCommandBuilder()
+        .setName(this.name)
+        .setDescription(this.description)
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+        .addUserOption((option) =>
+            option
+                .setName("user")
+                .setDescription("Personen du vill varna")
+                .setRequired(true),
+        )
+        .addStringOption((option) =>
+            option
+                .setName("reason")
+                .setDescription("Anledning till varningen")
+                .setRequired(true),
+        );
+    async execute(interaction: CommandInteraction) {
+        const member = interaction.options.get("user", true)
+            .member as GuildMember;
+        const reason = interaction.options.get("reason", true).value as string;
+        const has_sent_message = await this.warn(
+            member,
+            reason,
+            interaction.user.id,
+        );
+
+        const warn_embed = CreateModLogEmbed(
+            "warn",
+            `${member.user.username} har blivit varnad`,
+            reason,
+            this.name,
+            interaction,
+            has_sent_message
+        );
+
+        await interaction.editReply({ embeds: [warn_embed] });
+    }
+    async warn(member: GuildMember, reason: string, author_id: string) {
+        const profile_data = await GamerBotAPIInstance.models.get_profile_data(
+            member.user.id,
+        );
+
+        const mod_log = new ModLog(
+            "warn",
+            member.user.id,
+            member.user.username,
+            reason,
+            null,
+            Date.now(),
+            author_id,
+        );
+        profile_data.modLogs.push(mod_log);
+        profile_data.save();
+
+        let has_sent_message = true;
+        
+        await member.send(
+            `Du har fått en regelpåminnelse från SGC.\nPåminnelsen lyder: **${reason}**`,
+        ).catch(() => has_sent_message = false);
+
+        return has_sent_message;
+    }
+}
