70f2976d8e7d95dcb11ba4f6098da51ee9b8d359
lukasabbe <67807954+lukasabbe@users.noreply.github.com>
2025-01-22 21:41:33 +0100

    fixed debug problem

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ dot_env.config()
 
 const TOKEN = process.env.TOKEN
 const GAMERBOT_API_TOKEN = process.env.GAMERBOT_API_TOKEN as string
-const API_DEBUG_LOCAL = process.env.DEBUG as unknown as boolean;
+const API_DEBUG_LOCAL = process.env.DEBUG?.toLowerCase() === 'true';
 
 export const _dirname = path.join(dirname(fileURLToPath(import.meta.url)));
 
