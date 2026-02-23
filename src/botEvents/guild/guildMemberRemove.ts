e5e1e5409cd653d7037afe5c1cf30ae74cedea69
lukasabbe <67807954+lukasabbe@users.noreply.github.com>
2026-02-23 20:52:55 +0100

    Removed Checkin command

--- src/botEvents/guild/guildMemberRemove.ts
+++ src/botEvents/guild/guildMemberRemove.ts
@@ -2,6 +2,7 @@ import { Client, GuildMember } from "discord.js";
 import { Event } from "../../classes/event";
 
 export default class GuildMemberRemove implements Event {
+    constructor() {}
     async runEvent(client: Client, member: GuildMember) {
         const TRUSTED_ROLES_ID = ["818809151257575464", "872157696709783552", "821059798270214176", "1091356358890225674"];
         const CHANNEL_ID = "948330068575391784";
